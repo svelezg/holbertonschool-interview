c3d973c14edd43af7ecd763b8de2692d1b1f0daa
svelezg <svelez.velezgarcia@gmail.com>
2020-06-12 10:01:31 -0500

    Task is palindrome

---
+++ 0x08-palindrome_integer/palindrome.h
@@ -0,0 +1,10 @@
+#ifndef INC_0X08_PALINDROME_INTEGER_PALINDROME_H
+#define INC_0X08_PALINDROME_INTEGER_PALINDROME_H
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int is_palindrome(unsigned long n);
+
+
+#endif
